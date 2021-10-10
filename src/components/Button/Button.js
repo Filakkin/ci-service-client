c61237cbd79afd1433da1ec563dc6f8dad9c5460
filakkin <filakkin@gmail.com>
2021-10-11 06:23:42 +0700

    Добавит обработку нажатия в компонент кнопки

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import './Button.scss';
 import Icons from './Icons/Icons.js';
 
-const Button = ({ text, icon, size, isPrimary, disabled }) => {
+const Button = ({ text, icon, size, isPrimary, disabled, onClick }) => {
     const buttonClass = classNames({
         'Button': true,
         [`Button-${size}`]: true,
@@ -11,7 +11,7 @@ const Button = ({ text, icon, size, isPrimary, disabled }) => {
     });
     var Icon = Icons[icon];
 
-    return <button className={buttonClass} disabled={disabled}>
+    return <button className={buttonClass} disabled={disabled} onClick={onClick}>
         {Icon && <Icon />}
         {!!text && <div className='Button_text'>
             {text}
