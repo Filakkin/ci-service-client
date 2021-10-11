28030d26ac64e5192ba16323de6baafb3053aff7
filakkin <filakkin@gmail.com>
2021-10-11 16:36:37 +0700

    Подключит redux-store

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './App.scss';
 import StartScreen from './pages/startScreen';
@@ -14,42 +15,45 @@ import FooterMenu from './components/Footer/FooterMenu';
 import Copyright from './components/Footer/Copyright';
 import Link from './components/Link/Link';
 import AppContext from './AppContext';
+import store from './app/store';
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {settings: {interval: 10}});
+  const [state, dispatch] = useReducer(reducer, { settings: { interval: 10 } });
 
 
   return (
-    <Router>
-      <div className='App'>
-        <Switch>
-          <AppContext.Provider value={{state, dispatch}}>
-            <Route exact path="/">
-              {!!state?.settings?.repository ? <BuildHistory /> : <StartScreen />}
-            </Route>
-            <Route path="/settings">
-              <Settings />
-            </Route>
-          </AppContext.Provider>
-        </Switch>
-      </div>
-      <Footer>
-        <FooterMenu>
-          <Link url="#" text='Support' />
-          <Link url="#" text='Learning' />
-          <Link url="#" text='Русская версия' />
-        </FooterMenu>
-        <Copyright holder='Kalifkin Stanislav' />
-      </Footer>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className='App'>
+          <Switch>
+            <AppContext.Provider value={{ state, dispatch }}>
+              <Route exact path="/">
+                {!!state?.settings?.repository ? <BuildHistory /> : <StartScreen />}
+              </Route>
+              <Route path="/settings">
+                <Settings />
+              </Route>
+            </AppContext.Provider>
+          </Switch>
+        </div>
+        <Footer>
+          <FooterMenu>
+            <Link url="#" text='Support' />
+            <Link url="#" text='Learning' />
+            <Link url="#" text='Русская версия' />
+          </FooterMenu>
+          <Copyright holder='Kalifkin Stanislav' />
+        </Footer>
+      </Router>
+    </Provider>
   );
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
-      case 'save_settings':
-          return { ...state, settings: action.value };
-      default:
+    case 'save_settings':
+      return { ...state, settings: action.value };
+    default:
   }
 }
 
