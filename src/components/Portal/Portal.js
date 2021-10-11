862396bca98c5d73186d0c7db1cc59fbc02f210b
filakkin <filakkin@gmail.com>
2021-10-11 12:41:39 +0700

    Добавит компонент портала

---
+++ src/components/Portal/Portal.js
@@ -0,0 +1,12 @@
+import { createPortal } from "react-dom";
+import { usePortal } from "./usePortal"
+
+
+const Portal = ({children, parentId}) => {
+    const target = usePortal(parentId);
+    
+    return createPortal(children, target);
+}
+
+
+export default Portal;
---
+++ src/components/Portal/usePortal.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+export const usePortal = (parentId) => {
+    const elementRef = useRef(null);
+
+    const getElementRef = () => {
+        if (!elementRef.current) {
+            elementRef.current = document.createElement('div') ;
+        }
+
+        return elementRef.current;
+    }
+
+    useEffect(() => {
+        const parent = document.querySelector(`#${parentId}`);
+        parent.appendChild(getElementRef());
+
+        return () => getElementRef().remove();
+    }, [parentId]);
+
+
+    return getElementRef();
+}
